d1503f0b05226ce74af3d8c72fc9fb4fed98bd82
20DDan <dan.ratiu85@gmail.com>
2022-05-05 21:17:56 +0300

    All done in class so far.

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        RegisterTest Register = new RegisterTest();
         LoginTest login = new LoginTest();
+        Register.validRegisterTest();
         login.loginWithValidCredentialsTest();
         login.loginWithInvalidEmail();
         login.loginWithInvalidPasswordTest();
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,6 +9,15 @@ public class RegisterTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test/");
-
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.id("firstname")).sendKeys("Gusti");
+        driver.findElement(By.id("middlename")).sendKeys("Gigi");
+        driver.findElement(By.id("lastname")).sendKeys("Gog");
+        driver.findElement(By.cssSelector("#email_address")).sendKeys("Gusti.Gigi.Gog@Google.com");
+        driver.findElement(By.cssSelector("#password")).sendKeys("123456");
+        driver.findElement(By.cssSelector("#confirmation")).sendKeys("123456");
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
+        driver.close();
     }
 }
